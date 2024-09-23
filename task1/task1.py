a761956822b38855fc847a96939d05db2379d92d
Мальцева Наталья Витальевна <n.maltseva2@main.nsu.ru>
2024-09-23 10:11:59 +0700

    Решила первые 3 задачи

--- task1/task1.py
+++ task1/task1.py
@@ -1,1 +1,4 @@
-
+a, b = int(input()), int(input())
+a = (a + 1) // 2
+b = (b + 1) // 2
+print(abs(a - b))
--- task3/task3.py
+++ task3/task3.py
@@ -1,1 +1,6 @@
-
+n = int(input())
+a = n ** 0.5 // 1
+for i in range(int(a), 0, -1):
+    if(n % (i * i) == 0):
+        print(i * i)
+        break
